562c5ad0fbd6c4a95eb2d150491e69d68701079f
MiguelPerez <miguel.i.perez.p@gmail.com>
2018-09-30 21:46:42 -0500

    Add stuff for forms

---
+++ UmbracoTest/App_Code/ContactFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+ /// <summary>
+    /// Summary description for ContactFormModel
+    /// </summary>
+    public class ContactFormModel
+    {
+        public string Name { get; set; }
+        public int Email { get; set; }
+        public int Message { get; set; }
+        public ContactFormModel()
+        {
+        }
+    }
